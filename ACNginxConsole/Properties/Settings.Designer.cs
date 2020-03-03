e1aaa6b040770164dd08826c2097f41ba8a898ce
LogCreative <61653082+LogCreative@users.noreply.github.com>
2020-03-03 13:39:04 +0800

    Monitor Function

--- ACNginxConsole/Properties/Settings.Designer.cs
+++ ACNginxConsole/Properties/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace ACNginxConsole.Properties {
                 this["fulltest"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CloseProtection {
+            get {
+                return ((bool)(this["CloseProtection"]));
+            }
+            set {
+                this["CloseProtection"] = value;
+            }
+        }
     }
 }
